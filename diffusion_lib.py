21304f87e545f7c85fdcfaafc6aeff9714cc88fe
gaboraya@gmail.com <gaboraya@gmail.com>
2022-12-03 02:23:09 +0100

    add the Gaussian Diffusion process

--- diffusion_lib.py
+++ diffusion_lib.py
@@ -1,25 +1,23 @@
 """
-Abstract Class fro the forward and reverse process.
-"""
+Abstract Class for the forward and reverse process.
 
+    It explains what we need in the diffusion process
+"""
 import abc
 import torch
-import numpy as np
 
-
-# explain what we need in the diffusion process
 
 class DiffusionProcess(abc.ABC):
     """ Diffusion process abstract class. Functions are designed for a mini-batch of inputs """
 
-    def __init__(self, N):
+    def __init__(self, T):
         """ Construct a Discrete Diffusion process.
 
         Args:
             N: number of time steps
         """
         super().__init__()
-        self.N = N
+        self.N = T
 
     @property
     @abc.abstractmethod
@@ -27,13 +25,13 @@ class DiffusionProcess(abc.ABC):
         """End time of the DP."""
         pass
 
-    # @abc.abstractmethod
-    # def sde(self, x, t):
-    #     pass
+    @abc.abstractmethod
+    def transition_prob(self, x, t):
+        pass
 
     @abc.abstractmethod
-    def marginal_prob(self, x, t):
-        """ Parameters to determine the marginal distribution of the SDE, $p_t(x)$."""
+    def t_step_transition_prob(self, x, t):
+        """Computes the the t-step forward distribution q(x_t|x_0) """
         pass
 
     @abc.abstractmethod
@@ -41,93 +39,21 @@ class DiffusionProcess(abc.ABC):
         """Generate one sample from the prior distribution, $p_T(x)$."""
         pass
 
-    @abc.abstractmethod
-    def prior_logp(self, z):
-        """Compute log-density of the prior distribution.
 
-        Useful for computing the log-likelihood via probability flow ODE.
-
-        Args:
-            z: latent code
-        Returns:
-            log probability density
-        """
-        pass
-
-    def discretize(self, x, t):
-        """Discretize the SDE in the form : $x_{i+1} = x_i + f_i(x_i) + G_i z_i$
-
-        Useful for reverse diffusion sampling and probability flow sampling.
-        Defaults to Euler-Maruyama discretization.
+class GaussianDiffusion(DiffusionProcess):
+    def __init__(self, beta_min=0.1, beta_max=20, T=1000):
+        """Construct a Discrete Gaussian diffusion model.
 
         Args:
-            x: a torch tensor
-            t: a torch float representing the time step (from o to `self.T`).
-        Returns:
-            f, G
-        """
-        dt = 1 / self.N
-        drift, diffusion = self.sde(x, t)
-        f = drift * dt
-        G = diffusion * torch.sqrt(torch.tensor(dt, device=t.device))
-        return f, G
-
-    def reverse(self, score_fn, probability_flow=False):
-        """ Create the reverse-time SDE/ODE.
-
-        Args:
-            score_fn: A time-dependent score-based model that takes x and t and returns the score
-            probability_flow: If `True`, create the reverse-time ODE used for probability flow sampling
-        Returns:
-            the reverse SDE class
-        """
-        N = self.N
-        T = self.T
-        sde_fn = self.sde
-        discretize_fn = self.discretize
-
-        # Build the class for reverse-time SDE.
-        class RSDE(self.__class__):
-            def __init__(self):
-                self.N = N
-                self.probability_flow = probability_flow
-
-            @property
-            def T(self):
-                return T
-
-            def sde(self, x, t):
-                """Creates the drift and diffusion functions for the reverse SDE/ODE."""
-                drift, diffusion = sde_fn(x, t)
-                score = score_fn(x, t)
-                drift = drift - diffusion[:, None, None, None] ** 2 * score * (0.5 if self.probability_flow else 1.)
-                # Set the diffusion function to zero for ODEs.
-                diffusion = 0. if self.probability_flow else diffusion
-                return drift, diffusion
-
-            def discretize(self, x, t):
-                f, G = discretize_fn(x, t)
-                rev_f = f - G[:, None, None, None] ** 2 * score_fn(x, t) * (0.5 if self.probability_flow else 1.)
-                rev_G = torch.zeros_like(G) if self.probability_flow else G
-                return rev_f, rev_G
-
-        return RSDE()
-
-
-class DDPM(DiffusionProcess):
-    def __init__(self, beta_min=0.1, beta_max=20, N=1000):
-        """Construct a Discrete diffusion model.
-
-        Args:
-            beta_max: value of beta(0)
+            beta_min: value of beta(0)
             beta_max: value of beta(1)
-            N: number of discretization steps
+            T: number of timesteps
         """
-        super().__init__(N)
+        super().__init__(T)
         self.beta_0 = beta_min
         self.beta_1 = beta_max
-        self.N = N
-        self.discrete_betas = torch.linspace(beta_min / N, beta_max / N, N)
+        self.N = T
+        self.discrete_betas = torch.linspace(beta_min, beta_max, T)
         self.alphas = 1. - self.discrete_betas
         self.alphas_cumprod = torch.cumprod(self.alphas, dim=0)
         self.sqrt_alphas_cumprod = torch.sqrt(self.alphas_cumprod)
@@ -135,35 +61,28 @@ class DDPM(DiffusionProcess):
 
     @property
     def T(self):
-        return 1
+        return self.N
 
-    def marginal_prob(self, x, t):
-        log_mean_coeff = -0.25 * t ** 2 * (self.beta_1 - self.beta_0) - 0.5 * t * self.beta_0
-        mean = torch.exp(log_mean_coeff[:, None, None, None]) * x
-        std = torch.sqrt(1. - torch.exp(2. * log_mean_coeff))
+    def transition_prob(self, x, timestep):
+        """"Forward step as a result of the forward transition distribution q(x_t|x_t-1)
+            q(x_t|x_{t-1}) = N(x_t|\sqrt{1-beta_t}x_{t-1}, \beta_t*I)
+
+        Args:
+            x : tensor NxCxHxW in range [-1,1]
+            timestep: 1D tensor of size N [1,2,..., T]
+        """
+        beta = self.discrete_betas.to(x.device)[timestep]
+        mean = torch.sqrt(1 - beta[:, None, None, None].to(x.device)) * x
+        std = torch.sqrt(beta)
         return mean, std
 
-    def transition_prob(self, x, t):
-        """computes the discrete version as DDPM"""
+    def t_step_transition_prob(self, x, t):
+        """Computes the the t-step forward distribution q(x_t|x_0)
+            q(x_t|x_0) = \mathcal{N}(x_t; sqrt{\bar{\alpha_t}}x_0, (1-\bar{\alpha_t})I)
+        """
         mean = self.sqrt_alphas_cumprod.to(x.device)[t, None, None, None] * x
         std  = self.sqrt_1m_alphas_cumprod.to(x.device)[t]
         return mean, std
 
     def prior_sampling(self, shape):
         return torch.randn(*shape)
-
-    def prior_logp(self, z):
-        shape = z.shape
-        N = np.prod(shape[1:])
-        logps = -N / 2. * np.log(2 * np.pi) - torch.sum(z ** 2, dim=(1, 2, 3)) / 2.
-        return logps
-
-    def discretize(self, x, t):
-        """DDPM discretization."""
-        timestep = (t * (self.N - 1) / self.T).long()
-        beta = self.discrete_betas.to(x.device)[timestep]
-        alpha = self.alphas.to(x.device)[timestep]
-        sqrt_beta = torch.sqrt(beta)
-        f = torch.sqrt(alpha)[:, None, None, None] * x
-        G = sqrt_beta
-        return f, G
